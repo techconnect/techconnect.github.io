d675686e699a132cb4c06e5af0542147a6823cfd
ishanjoshi99 <ishan.joshi99@yahoo.com>
2014-09-06 07:46:52 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -97,5 +97,5 @@ $("#ea").click(function(){
   //m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   //})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-54492551-1', 'auto');
-  ga('send', 'pageview');
+  //ga('create', 'UA-54492551-1', 'auto');
+ // ga('send', 'pageview');
